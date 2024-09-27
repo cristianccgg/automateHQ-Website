f5a011e8627aef6c7ee67136b7bc19b4bca4a07d
cristianccgg <cristianccggg@gmail.com>
2024-09-27 17:40:09 -0500

    Navbar still

--- src/javascript/navbar.js
+++ src/javascript/navbar.js
@@ -1,18 +1,29 @@
-// Obtener los elementos del DOM
 const hambIcon = document.getElementById("hambIcon");
 const closeIcon = document.getElementById("closeIcon");
 const menu = document.getElementById("menu");
+const menuDesktop = document.getElementById("menu-desktop");
 
-// Agregar un event listener al ícono hamburguesa
+// Función para verificar si la pantalla es lo suficientemente ancha
+const isDesktop = () => window.innerWidth >= 768; // Ajusta el tamaño según tus necesidades
+
 hambIcon.addEventListener("click", () => {
-  menu.classList.toggle("hidden"); // Mostrar/ocultar el menú
+  menu.classList.toggle("hidden"); // Muestra el menú móvil
+  if (isDesktop()) {
+    // Solo mostrar menú desktop si es una pantalla de escritorio
+    menuDesktop.classList.remove("hidden"); // Asegúrate de que el menú no esté oculto
+    menuDesktop.classList.add("flex"); // Mostrar el menú desktop
+  }
   hambIcon.classList.add("hidden"); // Ocultar ícono hamburguesa
   closeIcon.classList.remove("hidden"); // Mostrar ícono de cerrar
 });
 
-// Agregar un event listener al ícono de cerrar
 closeIcon.addEventListener("click", () => {
-  menu.classList.toggle("hidden"); // Mostrar/ocultar el menú
+  menu.classList.add("hidden"); // Oculta el menú móvil
+  if (isDesktop()) {
+    // Solo ocultar menú desktop si es una pantalla de escritorio
+    menuDesktop.classList.remove("flex"); // Ocultar menú desktop
+    menuDesktop.classList.add("hidden"); // Asegúrate de que el menú esté oculto
+  }
   hambIcon.classList.remove("hidden"); // Mostrar ícono hamburguesa
   closeIcon.classList.add("hidden"); // Ocultar ícono de cerrar
 });
